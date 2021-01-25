ba3aafd1e9cf794f3273c963026a208bceff2303
otyamura <suzuki08suzuki@gmail.com>
2021-01-26 03:10:50 +0900

    fix wait

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, abort
-
-
+import json
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -10,16 +9,17 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
+from linebot.exceptions import LineBotApiError
 import os
 import getter as g
 
 # heroku上では使わない
-# from dotenv import load_dotenv
-# load_dotenv()
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 
-#環境変数取得
+# 環境変数取得
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
 YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
@@ -46,16 +46,22 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    # flag, updateList = g.run()
-    # list = '\n'.join(updateList)
-    # if flag:
-    #     message = "更新されたよ！\n"
-    # else:
-    #     message = "更新されたものはないよ！"
+    flag, updateList = g.run()
+    list = '\n'.join(updateList)
+    if flag:
+        message = "更新されたよ！\n"
+    else:
+        message = "更新されたものはないよ！"
     message = 'test'
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=message))
+        TextSendMessage(text='計測中…'))
+    # userIdが変化するのであればこれ
+    # userId = json.loads(str(event.source))
+    # print(userId['userId'])
+    # userId = userId['userId']
+    userId = 'Ucb74038bc5b24a5a36f6f98b0a470744'
+    line_bot_api.push_message(userId, TextSendMessage(text=message))
     # line_bot_api.reply_message(
     #     event.reply_token,
     #     TextSendMessage(text=message + list))
